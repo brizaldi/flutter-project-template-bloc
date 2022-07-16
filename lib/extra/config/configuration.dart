55c54a9c51137f7fb19747d50ae7cccde4a776b4
Bahri Rizaldi <bahri.rizaldi13@gmail.com>
2022-07-16 19:50:55 +0700

    Get app flavor from compile-time variable

--- lib/extra/config/configuration.dart
+++ lib/extra/config/configuration.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 
 import '../../features/auth/infrastructure/auth_interceptor.dart';
